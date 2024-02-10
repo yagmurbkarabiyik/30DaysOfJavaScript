efa6dc9a3b4f713690f4d951120f645d524af4fd
Yagmur Karabiyik <yagmurbkarabiyik@gmail.com>
2024-02-10 16:27:58 +0300

    day 3 completed

--- Day3/exercises.js
+++ Day3/exercises.js
@@ -89,9 +89,124 @@ console.log(date.getTime() / 1000)
 // console.log(total)
 
 //3
-let length  = prompt("Enter the length ")
-let width = prompt("Enter the width ")
-length = parseFloat(length)
-width = parseFloat(width)
-let rectangle = (length + width) * 2
-console.log(rectangle)
+// let length  = prompt("Enter the length ")
+// let width = prompt("Enter the width ")
+// length = parseFloat(length)
+// width = parseFloat(width)
+// let rectangle = (length + width) * 2
+// console.log(rectangle)
+
+//4
+// let radius = prompt('Please enter a radius')
+// let calculate = radius * 2 * Math.PI
+// console.log(calculate)
+
+//5
+// let x = prompt('Please enter x')
+// let calculateScope = (2 * x) - 2
+// console.log('y is : ', calculateScope)
+
+//6
+// let x1 = 2
+// let y1 = 2
+// let x2 = 6
+// let y2 = 10
+// let slope = (y2 - y1) / (x2 - x1)
+// console.log(slope)
+
+//7
+// if (calculateScope > slope) {
+//     console.log('First slope is greater than second')
+// }
+// else{
+//     console.log('Second slope is greater than first slope')
+// }
+
+//8
+// function calculateY(x) {
+//     return x*x + 6*x + 9;
+// }
+
+// const xValues = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5];
+// xValues.forEach(x => {
+//     const y = calculateY(x);
+//     console.log(`When x = ${x}, y = ${y}`);
+// })
+
+//9
+// let hours = prompt('Please enter work hours')
+// let ratePerHour = prompt('Please enter a rate per hour')
+// let earning = hours * ratePerHour
+// console.log(earning)
+
+//10
+// let getName = prompt('Please enter your name')
+// let getSurname = prompt('Please enter your surname')
+
+//     if (getName.length > 7 ) {
+//         console.log('Your name is long')
+//     }
+//     else{
+//         console.log('Your name is short')
+//     }
+
+//11
+/*let getFirstName = prompt('Enter a first name')
+let getLastName = prompt('Enter a last name')
+getFirstName.length > getLastName.length ? console.log(`Your first name  ${getFirstName} is longer than your family name ${getLastName}`) 
+                                         : console.log(`Your last name  ${getLastName} is longer than your first name ${getFirstName}`)*/
+
+//12
+// let myAge = prompt('Please enter my age')
+// let yourAge = prompt('Please enter your age')
+// let calculateAge = myAge - yourAge
+// let calculateAge2 = yourAge - myAge
+
+// if (myAge > yourAge) {
+//     console.log(`I am ${calculateAge} years older than you`)
+// }
+// else{
+//     console.log(`You are ${calculateAge2} years older than me`)
+// }
+
+//13
+/*let birthYear = prompt('enter birth year')
+ let date = new Date()
+ let dateCheck = date.getFullYear()
+ let currentAge = dateCheck - birthYear
+ let driveAge = 18 - currentAge
+ currentAge >= 18
+     ? console.log(`You are ${currentAge}. You are old enough to drive`)
+     : console.log(`You are ${currentAge}. You will be allowed to drive after ${driveAge} years.`)*/
+
+//14
+// let numberOfYears = prompt('Enter a number of years')
+// let calSeconds = 31536000 * numberOfYears
+// console.log(calSeconds)
+
+//15
+let year = date.getFullYear(),
+month = date.getMonth(),
+day = date.getDate(),
+hour = date.getHours(),
+minute = date.getMinutes(),
+second = date.getSeconds(),
+dayNumber = date.getDay()
+
+const twoDigitMonth = `0${month + 1}`.slice(-2);
+const twoDigitDay = `0${day + 1}`.slice(-2)
+const towDigitHour = `0${hour}`.slice(-2)
+const twoDigitMinute = `0${minute + 1 }`.slice(-2)
+
+let humanReadableDate1 = `${year}-${twoDigitMonth}-${twoDigitDay} ${hour}:${minute}`
+let humanReadableDate2 = `${twoDigitDay}-${twoDigitMonth}-${year} ${hour}:${minute}`
+let humanReadableDate3 = `${twoDigitDay}/${twoDigitMonth}/${year} ${hour}:${minute}`
+
+console.log(humanReadableDate1)
+console.log(humanReadableDate2)
+console.log(humanReadableDate3)
+
+//Level 3 Exercises
+//1
+let humanReadableDate4 = `${year}-${twoDigitMonth}-${day} ${towDigitHour}:${twoDigitMinute}`
+console.log(humanReadableDate4)
