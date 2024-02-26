445ebc359165dee8f601ef9fa81fb7bea1973d13
Yagmur Karabiyik <yagmurbkarabiyik@gmail.com>
2024-02-26 12:07:47 +0300

    day 20 completed

---
+++ Day20/cleanCode.js
@@ -0,0 +1,10 @@
+/*
+- camelCase for variables and functions
+- choose to const for constants, arrays, objects and functions. Instead of double quote, we 
+choose to use single quote or backtick.
+- also removed semicolons 
+- space around arithmetcic operators, assignment operators and after comma
+- arrow function instead of function declaration(If we need to this keyword we should use function declaration bcs arrow 
+    function does not have this keyword)
+- make array names plural (numbers, countries, skills)
+*/
